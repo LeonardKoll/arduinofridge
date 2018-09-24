b93217fbbede73bfa6befdce955d49895f435929
leonardkoll <kcommunicate@outlook.com>
2018-09-24 15:54:22 +0200

    structure improved

--- src/ownConnect/send/send.ino
+++ src/ownConnect/send/send.ino
@@ -15,14 +15,21 @@ void loop()
   // 5 seconds of Heartbeat
   for (int  i=0; i<3; i++)
   {
-    digitalWrite(outPin, HIGH);
-    delay(2);
-    digitalWrite(outPin, LOW);
+    heartbeat();
     delay (1500);
   }
 
 
   Serial.print("Sending\n");
+  // Start sending
+  int sendData[8] = {1,1,1,0,0,1,1,1};
+  send(sendData, 8);
+  
+  delay(5000);
+}
+
+void send(int msg[], int msgLength)
+{
   // Start sending
   digitalWrite(outPin, HIGH);
   delay(1);
@@ -30,24 +37,26 @@ void loop()
   delay(1);
 
   // Data
+  for (int i=0; i<msgLength; i++)
+  {
+    if(msg[i] == 0)
+    {
+      digitalWrite(outPin, LOW);
+    }
+    else
+    {
+      digitalWrite(outPin, HIGH);
+    }
+    delay(1);
+  }
+
   digitalWrite(outPin, LOW);
-  delay(1);
-  digitalWrite(outPin, HIGH);
-  delay(1);
-  digitalWrite(outPin, LOW);
-  delay(1);
-  digitalWrite(outPin, LOW);
-  delay(1);
-  digitalWrite(outPin, LOW);
-  delay(1);
-  digitalWrite(outPin, HIGH);
-  delay(1);
-  digitalWrite(outPin, HIGH);
-  delay(1);
-  digitalWrite(outPin, HIGH);
-  delay(1);
-  Serial.print("Sending done.\n");
-  
-  digitalWrite(outPin, LOW);
-  delay(5000);
 }
+
+void heartbeat ()
+{
+  digitalWrite(outPin, HIGH);
+  delay(2);
+  digitalWrite(outPin, LOW);
+}
+
